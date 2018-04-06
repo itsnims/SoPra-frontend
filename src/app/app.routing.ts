8789f1191f6bdef2526459b5f12044a3a60effe2
acharl <amadeo@fermat.org>
2018-04-06 16:21:50 +0200

    added route for host screen component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,11 +2,13 @@
 import {LoginComponent} from './login/login.component';
 import {GameComponent} from './game/game.component';
 import {AuthGuardService} from './shared/services/auth-guard.service';
+import {HostScreenComponent} from './host-screen/host-screen.component';
 
 
 const appRoutes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'game', component: GameComponent}, // this works when we omit "canActivate: [AuthGuardService]"
+    { path: "host-screen", component: HostScreenComponent},
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
