9ec08145af60eedfb33fad796f479892029fdda5
acharl <amadeo@fermat.org>
2018-04-06 16:51:01 +0200

    add route for in-game-screen #33

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,6 +4,7 @@ import {GameComponent} from './game/game.component';
 import {AuthGuardService} from './shared/services/auth-guard.service';
 import {HostScreenComponent} from './host-screen/host-screen.component';
 import {WaitingScreenComponent} from './waiting-screen/waiting-screen.component';
+import {InGameScreenComponent} from './in-game-screen/in-game-screen.component';
 
 
 const appRoutes: Routes = [
@@ -11,6 +12,7 @@ const appRoutes: Routes = [
     { path: 'game', component: GameComponent}, // this works when we omit "canActivate: [AuthGuardService]"
     { path: 'host-screen', component: HostScreenComponent},
     { path: 'waiting-screen', component: WaitingScreenComponent},
+    { path: 'in-game-screen', component: InGameScreenComponent},
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
