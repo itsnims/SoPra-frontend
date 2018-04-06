dcea288505a8c03e5bae9931a6ad73886082d856
acharl <amadeo@fermat.org>
2018-04-06 16:15:06 +0200

    added host-screen component

---
+++ src/app/host-screen/host-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-host-screen',
+  templateUrl: './host-screen.component.html',
+  styleUrls: ['./host-screen.component.css']
+})
+export class HostScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
