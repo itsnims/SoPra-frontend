507e3d623621a5dab75725d65ae6e7dbd898ee4d
acharl <amadeo@fermat.org>
2018-04-06 16:35:49 +0200

    corrected route to waiting screen component #32

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,13 +3,14 @@ import {LoginComponent} from './login/login.component';
 import {GameComponent} from './game/game.component';
 import {AuthGuardService} from './shared/services/auth-guard.service';
 import {HostScreenComponent} from './host-screen/host-screen.component';
+import {WaitingScreenComponent} from './waiting-screen/waiting-screen.component';
 
 
 const appRoutes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'game', component: GameComponent}, // this works when we omit "canActivate: [AuthGuardService]"
     { path: 'host-screen', component: HostScreenComponent},
-    { path: 'waiting-screen', component: HostScreenComponent},
+    { path: 'waiting-screen', component: WaitingScreenComponent},
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
