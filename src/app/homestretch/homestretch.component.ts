8f5c154b1dc6c0fbcacf87790ec932d8b98a5cfc
mariab19 <clara.barth@hotmail.de>
2018-05-18 04:49:50 +0200

    173 added specific TS for homestretch

--- src/app/homestretch/homestretch.component.ts
+++ src/app/homestretch/homestretch.component.ts
@@ -20,10 +20,21 @@ import {User} from '../shared/models/user';
   styleUrls: ['./homestretch.component.css']
 })
 export class HomestretchComponent implements OnInit, AfterViewInit {
+   hex: HexComponent;
 
-  /*classe für click events...
-  * */
-  hex: HexComponent;
+
+  old = 0;
+  new1 = 0;
+  remove: boolean;
+  count = 1;
+
+  BK1: boolean;
+  BK2: boolean;
+  BK3: boolean;
+  BK4: boolean;
+  BK5: boolean;
+
+
   empty: string;
   hello: any;
   players: PlayerComponent[] = [];
@@ -37,8 +48,14 @@ export class HomestretchComponent implements OnInit, AfterViewInit {
   playerName = JSON.parse(localStorage.getItem('currentUser')).name;
   [key: string]: any;
   blockade: any;
+
+
+
+
   Bstrenght: any;
   numberX: number;
+  list: any;
+  blockadeColour: string;
   /*blockadeColour: string;
   //blockadeStrength: string;
   //api + game + spec + blockades
@@ -47,6 +64,7 @@ export class HomestretchComponent implements OnInit, AfterViewInit {
   blockadelist: string[]= [];
   constructor(private componentFactoryResolver: ComponentFactoryResolver, private http: HttpClient) { }
   @ViewChildren(HexComponent) divs: QueryList<HexComponent>;
+
   ngOnInit() {
     localStorage.removeItem('mode')
 
@@ -82,7 +100,7 @@ export class HomestretchComponent implements OnInit, AfterViewInit {
 
       }
       console.log('players: ', this.players);
-      console.log('hello world')
+      console.log('hello world');
     }
     /*
     How many playing pieces should be displayed on the board as well as their initial positions*/
@@ -138,19 +156,37 @@ export class HomestretchComponent implements OnInit, AfterViewInit {
   }*/
     this.http.get(this.apiUrl + this.currentRoom + '/blockade')
       .subscribe(result => {
-        const first = this.blockadestring[0];
-        this[first] = 'hello';
-        for (let i = 0; i < 4; i++) {
-          const dummy = this.blockadestring[i];
-          this[dummy] = result[i].name;
+        console.log('blockade', result)
+        let list = [];
+        let iterable = 1;
+        for (const object in result) {
+          list.push({name: result[object].name, color: result[object].Color, strength: result[object].strenght});
+          // list.push(list['strength'] = result[object].strenght);
           /*this.Bstrenght[i] = result[i].strenght;*/
-        }
-        this.blockadelist.push(this.blockade0);
-        this.blockadelist.push(this.blockade1);
-        this.blockadelist.push(this.blockade2);
-        this.blockadelist.push(this.blockade3);
 
-        console.log('blockades', this.blockadelist);
+          console.log(String(iterable));
+          if (list[object].color.toLowerCase() === 'white'){
+            console.log(list[object].name)
+            this["colorBK" + iterable] = 'hexagon grey'
+          }else {
+            this["colorBK" + iterable] = 'hexagon ' + list[object].color.toLowerCase();
+          }
+          this["strengthBK" + iterable] = 'blockade' + list[object].strength
+          iterable ++;
+        }
+
+        // this.bk1Colour = 'hexagon ' + list[0].color.toLowerCase();
+        //
+        //       this.bk2Colour = 'hexagon ' + list[1].color.toLowerCase();
+        /*this.bk1Strength = 'blockade' + list[0].strength;
+        this.bk2Strength = 'blockade' + list[1].strength;
+        this.bk3Strength = 'blockade' + list[2].strength;
+        this.bk4Strength = 'blockade' + list[3].strength;
+*/
+
+        console.log('blockade', this.bk1Strength);
+        console.log(this.bk3Strength)
+
       });
 
 
@@ -158,7 +194,7 @@ export class HomestretchComponent implements OnInit, AfterViewInit {
 
 
   ngAfterViewInit() {
-    // this.divs.forEach(div => console.log(div));
+
     const hex = this.divs.forEach(item => {
       this.hexMapById.set(item.hexId, item);
       if (['B9', 'B11'].includes(item.hexId)) {
@@ -166,6 +202,7 @@ export class HomestretchComponent implements OnInit, AfterViewInit {
       }
       return false;
     });
+
     /**/
     /*highlights all potentialMoveIds*/
     console.log(this.hexMapById);
@@ -186,6 +223,39 @@ export class HomestretchComponent implements OnInit, AfterViewInit {
     this.hexMapById.get(this.players[1].position).addplayer(this.players[1]);
     this.hexMapById.get(this.players[2].position).addplayer(this.players[2]);
     /*this.hexMapById.get(this.players[3].position).addplayer(this.players[3]);*/
+  getCurrentBlockades() {
+    this.http.get(this.apiUrl + this.currentRoom + '/Currentblockade')
+      .subscribe(result => {
+        console.log('current', result)
+        let iter = 0
+        for (const object in result) {
+          console.log('current', object);
+          iter ++;
+        };
+        this.new1 = iter;
+        console.log('new', this.new1)
+        console.log('newold', this.old)
+
+        if (this.old === 0){this.old = iter}else {
+          if (this.old !== this.new1) {
+
+            this.remove = true;
+
+          } else {
+            this.remove = false;
+          }
+          console.log('newcount', this.count)
+          this.old = iter;
+
+          if (this.remove) {
+            this["BK" + this.count] = true;
+
+            console.log('BK1', this.BK1)
+            this.count ++;
+          }
+        }
+      })
+  }
 
 
   addPlayers(selectedCard: any, possibleTiles: any) {
@@ -200,22 +270,32 @@ export class HomestretchComponent implements OnInit, AfterViewInit {
     }
 
     if (possibleTiles.length <= 0){} else {
-
+      const blockades = ['BK1', 'BK2', 'BK3', 'BK4', 'BK5', 'BK6', 'BK7']
       // addPlayers(selected: Array<string>) {
-      if (this.blockadelist.indexOf(JSON.parse(localStorage.getItem('selectedHex'))) > -1) {
+      if (blockades.indexOf(JSON.parse(localStorage.getItem('selectedHex'))) > -1) {
+        console.log('in function');
+        console.log('BKSSS', localStorage.getItem('selectedHex'))
+        if (localStorage.getItem('selectedHex') === '"BK1"'){this.BK1 = true;}
+        if (localStorage.getItem('selectedHex') === '"BK2"'){this.BK2 = true;}
+        if (localStorage.getItem('selectedHex') === '"BK3"'){this.BK3 = true;}
+        if (localStorage.getItem('selectedHex') === '"BK4"'){this.BK4 = true;}
+        if (localStorage.getItem('selectedHex') === '"BK5"'){this.BK5 = true;}
+
+
+
+
         const httpOptions = {
           headers: new HttpHeaders({
             'Content-Type': 'application/json'
           })};
         this.selectedHex = JSON.parse(localStorage.getItem('selectedHex'));
 
-        /*
-        Send blockade to backend*/
-        console.log('send to backend: ', this.apiUrl + this.currentRoom + '/' + this.playerName + '/' + selectedCard + '/blockade');
         this.http.put(this.apiUrl + this.currentRoom + '/' + this.playerName + '/' + selectedCard + '/blockade', httpOptions).subscribe((result => console.log('result api crossblockade', result)));
         const element = document.getElementById(JSON.parse(localStorage.getItem('selectedHex')));
-        (<HTMLElement>element).remove()
-        this.removeChild(element);
+        (<HTMLElement>element).remove();
+        console.log()
+
+
       }
       else {
         this.tile = JSON.parse(localStorage.getItem('selectedHex'))
@@ -271,6 +351,7 @@ export class HomestretchComponent implements OnInit, AfterViewInit {
       this.hexMapById.get(tile).removehightlight();
 
     }}
+
   updatePosition(oldarray: any, newarray: any) {
     // console.log('in update');
     /*currently only for NOT 2players logic*/
@@ -296,10 +377,13 @@ export class HomestretchComponent implements OnInit, AfterViewInit {
       if (oldarray.length === 0) {
         this.currentkey = String(i) + 'st'
         localStorage.removeItem(this.currentkey + 'st');
-        this.hexMapById.get(newarray[i]).addplayer(this.players[i], newarray[i], this.empty);
         localStorage.setItem(this.currentkey, newarray[i]);
-      } else {
-        if ( oldarray[i] !== newarray[i]){
+        console.log(newarray[i])
+        console.log(this.hexMapById.get('B1'))
+        this.hexMapById.get(newarray[i]).addplayer(this.players[i], newarray[i], this.empty);
+        console.log('currentkey', this.currentkey, 'currentVal', localStorage.getItem(this.currentkey)) }
+      else {
+        if ( oldarray[i] !== newarray[i]) {
           this.currentkey = String(i) + 'st'
           localStorage.removeItem(this.currentkey + 'st');
           localStorage.setItem(this.currentkey, newarray[i]);
@@ -308,9 +392,12 @@ export class HomestretchComponent implements OnInit, AfterViewInit {
             console.log('in else/if new: ', newarray[i])
           // localStorage.setItem('currentTile', newarray[i]);
           /*remove player form hex component and then add them to the new positions.*/
+          this.getCurrentBlockades();
           this.hexMapById.get(oldarray[i]).removePlayer();
           console.log('this will be the player added: ', this.players[i])
-          this.hexMapById.get(newarray[i]).addplayer(this.players[i], newarray[i], this.empty);}
+          this.hexMapById.get(newarray[i]).addplayer(this.players[i], newarray[i], this.empty);
+          console.log('current after log');
+        }
         else {}
       }
     }
@@ -344,6 +431,4 @@ export class HomestretchComponent implements OnInit, AfterViewInit {
 
 
 }
-
-
 
